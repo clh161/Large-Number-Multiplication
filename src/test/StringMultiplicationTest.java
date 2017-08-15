2032d04c04238a4f7d0ffa4624b4d141ac0b525f
Jacob Ho <clh161@gmail.com>
2017-08-16 02:58:06 +0800

    Add test case

--- src/test/StringMultiplicationTest.java
+++ src/test/StringMultiplicationTest.java
@@ -62,6 +62,10 @@ public class StringMultiplicationTest {
             String sumSring = StringMultiplication.product(String.valueOf(a), String.valueOf(b));
             assertEquals(a + " x " + b, String.valueOf(a * b), sumSring);
         }
+        String a = "3141592653589793238462643383279502884197169399375105820974944592";
+        String b = "2718281828459045235360287471352662497757247093699959574966967627";
+        String sumSring = StringMultiplication.product(a, b);
+        assertEquals(a + " x " + b, "8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184", sumSring);
     }
 
     @Test
