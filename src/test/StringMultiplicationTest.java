a9aaef98af6cd623097faea17957af2f847b2eea
Jacob Ho <clh161@gmail.com>
2017-08-15 21:01:43 +0800

    Reduce redundancy

--- src/test/StringMultiplicationTest.java
+++ src/test/StringMultiplicationTest.java
@@ -12,11 +12,17 @@ public class StringMultiplicationTest {
     public void sum() throws Exception {
         Random r = new Random();
         for (int i = 0; i < 1000; i++) {
-            int a = r.nextInt((int) Math.sqrt(Integer.MAX_VALUE));
-            int b = r.nextInt((int) Math.sqrt(Integer.MAX_VALUE));
+            int a = getRandomIntMax();
+            int b = getRandomIntMax();
             String sumSring = StringMultiplication.sum(String.valueOf(a), String.valueOf(b));
             assertEquals(a + "+" + b, String.valueOf(a + b), sumSring);
         }
+    }
+
+
+    private int getRandomIntMax() {
+        Random r = new Random();
+        return r.nextInt((int) Math.sqrt(Integer.MAX_VALUE));
     }
 
 }
